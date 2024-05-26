323424a27f2f669c3c81ea1ce0dea880462d7afa
Tim Hatch <hatch.tim@gmail.com>
2024-05-26 12:53:56 +0100

    refactor(fractional): Alternate variable declaration
    Use d.Len() to keep length calculations consistently expressed
    Declare j as j := i + 1

--- rankings.go
+++ rankings.go
@@ -70,12 +70,15 @@ func OrdinalRank(d Rankable) []float64 {
 // e.g. 1, 2.5, 2.5, 4, 5
 func FractionalRank(d Rankable) []float64 {
 	r := make([]float64, d.Len())
-	for i := 0; i < len(r); {
-		var j int
+
+	for i := 0; i < d.Len(); {
+		j := i + 1
 		f := float64(i + 1)
-		for j = i + 1; j < len(r) && d.RankEqual(i, j); j++ {
+
+		for ; j < d.Len() && d.RankEqual(i, j); j++ {
 			f += float64(j + 1)
 		}
+
 		f /= float64(j - i)
 		for ; i < j; i++ {
 			r[i] = f
